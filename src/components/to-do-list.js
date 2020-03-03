72c1250575d43c027b38579ceacd1fee3a43179a
MartaHolovanova <marta.golovanova@gmail.com>
2020-03-03 21:39:26 +0200

    first commit

---
+++ src/components/to-do-list.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoListItem from "todo-list-item";
+
+const ToDoList = () => {
+    return (
+        <li>
+            <ul>{ToDoListItem}</ul>
+        </li>
+
+    )
+};
+
+export default ToDoList;
---
+++ src/components/todo-list-item.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ToDoListItem = () => {
+    return(
+        <span>Item</span>
+    )
+};
+
+export default ToDoListItem;
