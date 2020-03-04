7ca6fbbca2d799e82edfef24aa1750e8d29c56f6
МартаHolovanova <marta.golovanova@gmail.com>
2020-03-04 21:40:55 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchPanel from "./components/search-panel";
+import ToDoList from "./components/to-do-list";
+
 const App = () => {
     return(
-        <div>Hello</div>
+        <div>
+            < SearchPanel />
+            < ToDoList />
+        </div>
     )
 
 }
